d922198944f5bc96c4ee273f1348176184ad40af
Timur Mirgaliev <timir02@yandex.ru>
2021-02-14 16:21:58 +0300

    FirstWeek

---
+++ Dz_FirstWeek/cmake-build-debug/CMakeFiles/Dz_FirstWeek.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dz_FirstWeek.dir/main.cpp.obj"
+  "Dz_FirstWeek.exe"
+  "Dz_FirstWeek.exe.manifest"
+  "Dz_FirstWeek.pdb"
+  "libDz_FirstWeek.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dz_FirstWeek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
