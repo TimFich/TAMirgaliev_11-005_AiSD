b541c6991215f4a80e627a4e01db5ee9f5823dc3
Timur Mirgaliev <timir02@yandex.ru>
2021-05-03 20:26:44 +0300

    DZ(SixthWeek)

---
+++ Dz28.04.21/cmake-build-debug/CMakeFiles/Dz28_04_21.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/timir/CLionProjects/Dz28.04.21/Task2.cpp" "C:/Users/timir/CLionProjects/Dz28.04.21/cmake-build-debug/CMakeFiles/Dz28_04_21.dir/Task2.cpp.obj"
+  "C:/Users/timir/CLionProjects/Dz28.04.21/Task3.cpp" "C:/Users/timir/CLionProjects/Dz28.04.21/cmake-build-debug/CMakeFiles/Dz28_04_21.dir/Task3.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Dz28.04.21/cmake-build-debug/CMakeFiles/Dz28_04_21.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dz28_04_21.dir/Task2.cpp.obj"
+  "CMakeFiles/Dz28_04_21.dir/Task3.cpp.obj"
+  "Dz28_04_21.exe"
+  "Dz28_04_21.exe.manifest"
+  "Dz28_04_21.pdb"
+  "libDz28_04_21.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dz28_04_21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
