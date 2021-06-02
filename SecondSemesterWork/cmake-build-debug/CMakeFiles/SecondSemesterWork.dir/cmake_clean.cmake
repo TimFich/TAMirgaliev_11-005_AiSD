b773d78c39a9e96d78dafc5ab5b58287858596b9
Timur Mirgaliev <timir02@yandex.ru>
2021-06-02 22:02:50 +0300

    SecondSemestrWork

---
+++ SecondSemesterWork/cmake-build-debug/CMakeFiles/SecondSemesterWork.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SecondSemesterWork.dir/main.cpp.obj"
+  "SecondSemesterWork.exe"
+  "SecondSemesterWork.exe.manifest"
+  "SecondSemesterWork.pdb"
+  "libSecondSemesterWork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SecondSemesterWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
